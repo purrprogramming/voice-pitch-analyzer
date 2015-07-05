e07b79ee4e28195125048db7165a3f8b34bb634f
Felix Wittmann <mail@felixwittmann.de>
2015-07-05 19:03:08 +0200

    initial commit

--- app/src/main/java/lilithwittmann/de/voicepitchanalyzer/RecordViewFragment.java
+++ app/src/main/java/lilithwittmann/de/voicepitchanalyzer/RecordViewFragment.java
@@ -1,13 +1,15 @@
 package lilithwittmann.de.voicepitchanalyzer;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
 
 import lilithwittmann.de.voicepitchanalyzer.models.Recording;
 
@@ -21,32 +23,61 @@ import lilithwittmann.de.voicepitchanalyzer.models.Recording;
 public class RecordViewFragment extends Fragment {
 
     private static final String LOG_TAG = RecordViewFragment.class.getSimpleName();
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
     private OnFragmentInteractionListener mListener;
+    private Recording currentRecord;
 
     public RecordViewFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static RecordViewFragment newInstance(int sectionNumber, Recording recording) {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(Recording.KEY, recording);
+        bundle.putInt(ARG_SECTION_NUMBER, sectionNumber);
+
+        RecordViewFragment recordFragment = new RecordViewFragment();
+        recordFragment.setArguments(bundle);
+//        getSupportFragmentManager().beginTransaction().replace(R.id.container, recordFragment).commit();
+
+        return recordFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         if (savedInstanceState != null) {
-            Recording recording = (Recording) savedInstanceState.getSerializable(Recording.KEY);
-            Log.i(LOG_TAG, String.format("%s", recording.getDate()));
-            Log.i(LOG_TAG, String.format("Avg: %sHz", recording.getRange().getAvg()));
-            Log.i(LOG_TAG, String.format("Min: %sHz", recording.getRange().getMin()));
-            Log.i(LOG_TAG, String.format("Max: %sHz", recording.getRange().getMax()));
+            this.currentRecord = (Recording) savedInstanceState.getSerializable(Recording.KEY);
+//            Log.i(LOG_TAG, String.format("%s", recording.getDate()));
+//            Log.i(LOG_TAG, String.format("Avg: %sHz", recording.getRange().getAvg()));
+//            Log.i(LOG_TAG, String.format("Min: %sHz", recording.getRange().getMin()));
+//            Log.i(LOG_TAG, String.format("Max: %sHz", recording.getRange().getMax()));
         } else if (this.getArguments() != null) {
-            Recording recording = (Recording) this.getArguments().getParcelable(Recording.KEY);
-            Log.i(LOG_TAG, String.format("%s", recording.getDate()));
-            Log.i(LOG_TAG, String.format("Avg: %sHz", recording.getRange().getAvg()));
-            Log.i(LOG_TAG, String.format("Min: %sHz", recording.getRange().getMin()));
-            Log.i(LOG_TAG, String.format("Max: %sHz", recording.getRange().getMax()));
+            this.currentRecord = this.getArguments().getParcelable(Recording.KEY);
+//            Log.i(LOG_TAG, String.format("%s", recording.getDate()));
+//            Log.i(LOG_TAG, String.format("Avg: %sHz", recording.getRange().getAvg()));
+//            Log.i(LOG_TAG, String.format("Min: %sHz", recording.getRange().getMin()));
+//            Log.i(LOG_TAG, String.format("Max: %sHz", recording.getRange().getMax()));
         }
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_record_view, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        LineChart chart = (LineChart) view.findViewById(R.id.recording_chart);
+//        LineData data = new LineData(this.currentRecord.getRange().getStringPitches());
+//        LineDataSet dataset = new LineDataSet(this.currentRecord.getRange().getPitches());
+        super.onViewCreated(view, savedInstanceState);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
